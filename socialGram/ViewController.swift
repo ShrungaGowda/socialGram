2f5c5a413305dd2a5e43a7944c56563c70aef219
shrunga <shrunga26@gmail.com>
2018-03-14 20:32:32 -0700

    Initial Commit

---
+++ socialGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialGram
+//
+//  Created by Sushruth on 3/14/18.
+//  Copyright © 2018 iosDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
