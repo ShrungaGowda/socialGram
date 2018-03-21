4201e766ed31b140bac07fa0bacc913a47f32619
shrunga <shrunga26@gmail.com>
2018-03-20 20:00:08 -0700

    fb authentication added

--- Podfile
+++ Podfile
@@ -4,12 +4,12 @@ platform :ios, '9.0'
 target 'socialGram' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
-
+# Pods for socialGram
   pod 'Firebase/Core'
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
-
-  # Pods for socialGram
+  pod 'FBSDKLoginKit'
+  
 
 end
--- socialGram/Controller/SignInVC.swift
+++ socialGram/Controller/SignInVC.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 class SignInVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,34 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //SIGN IN USING FB LOGIN
+    @IBAction func fbSignTapped(_ sender: Any) {
+    //Assign login manager
+       let fbLogin = FBSDKLoginManager()
+    
+        fbLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in // login using email (fb)
+            if error != nil{                                                            //FB login error
+                print("LOGIN ERROR : Unable to authenticate with FB - \(String(describing: error))")
+            }else if result?.isCancelled == true {                                      //if user cancells it
+                print("LOGIN ERROR : User cancelled FB authentication")
+            }else{
+                print("LOGIN SUCCESSFUL :  Able to login with  FB")                             //Succesfully loging
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString) //using fb login we get auth token
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    //Create a generic method (same for GIT/Twitter/Insta using firebase) which takes credentials
+    func firebaseAuth(_ credential: AuthCredential){
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil{
+                print("LOGIN ERROR : Unable to login with firebase \(String(describing: error))")
+            }else{
+                 print("LOGIN SUCCESSFUL : Able to login with firebase")
+            }
+        }
+    }
+    
 }
 
